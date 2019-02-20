f2a7e5c4909af5fb3f97f65085848e97c93f2c3b
Corey Osman <corey@nwops.io>
2019-02-19 23:08:27 -0800

    Adds more support for rspec-puppet-facts

--- module_files/spec/spec_helper.rb
+++ module_files/spec/spec_helper.rb
@@ -7,33 +7,56 @@ include RspecPuppetFacts
 # Uncomment this to show coverage report, also useful for debugging
 #at_exit { RSpec::Puppet::Coverage.report! }
 
-# set to "yes" to enable strict variable checking, the equivalent of setting strict_variables=true in puppet.conf.
-#ENV['STRICT_VARIABLES'] = 'yes'
-
-# set to the desired ordering method ("title-hash", "manifest", or "random") to set the order of unrelated resources
-# when applying a catalog. Leave unset for the default behavior, currently "random". This is equivalent to setting
-# ordering in puppet.conf.
-#ENV['ORDERING'] = 'random'
-
-# set to "no" to enable structured facts, otherwise leave unset to retain the current default behavior.
-# This is equivalent to setting stringify_facts=false in puppet.conf.
-#ENV['STRINGIFY_FACTS']  = 'no'
-
-# set to "yes" to enable the $facts hash and trusted node data, which enabled $facts and $trusted hashes.
-# This is equivalent to setting trusted_node_data=true in puppet.conf.
-#ENV['TRUSTED_NODE_DATA'] = 'yes'
-
+# @return [String] - the path to the fixtures directory
 def fixtures_dir
   @fixtures_dir ||= File.join(File.dirname(__FILE__), 'fixtures')
 end
 
+# @return [String] = the path the directory of external facterdb facts
 def mock_facts
   @mock_facts ||= File.join(fixtures_dir, 'facterdb_facts')
 end
 
+# @return [Hash] - returns a hash of testable operating systems
+# uncomment and replace empty hash
+# modify to your liking, default to all supported
+# operating systems defined in the metadata.json file
+def test_on
+  @test_on ||= {}
+  # {hardwaremodels: ['x86_64'],
+  # supported_os: [
+  #     {
+  #         'operatingsystem' => 'Ubuntu',
+  #         'operatingsystemrelease' => ['14.04'],
+  #     },
+  # ]},
+end
+
 ENV['FACTERDB_SEARCH_PATHS'] = mock_facts
 
+default_facts = {
+    puppetversion: Puppet.version,
+    facterversion: Facter.version,
+}
+
+default_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_facts.yml'))
+default_module_facts_path = File.expand_path(File.join(File.dirname(__FILE__), 'default_module_facts.yml'))
+
+if File.exist?(default_facts_path) && File.readable?(default_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_facts_path)))
+end
+
+if File.exist?(default_module_facts_path) && File.readable?(default_module_facts_path)
+  default_facts.merge!(YAML.safe_load(File.read(default_module_facts_path)))
+end
+
 RSpec.configure do |c|
-    c.formatter = 'documentation'
-    c.mock_with :rspec
+  c.default_facts = default_facts
+  c.formatter = 'documentation'
+  c.mock_with :rspec
+  c.before :each do
+    # set to strictest setting for testing
+    # by default Puppet runs at warning level
+    Puppet.settings[:strict] = :warning
+  end
 end
